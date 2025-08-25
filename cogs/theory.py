0f7154429b815e54bfb60255c01a43e8c5368206
Bobbyperson <bobbyperson12345@gmail.com>
2025-08-25 13:59:31 -0400

    fix everyone exploit

--- cogs/theory.py
+++ cogs/theory.py
@@ -1,6 +1,7 @@
 import random
 
 import anyio
+import discord
 from discord.ext import commands
 
 from utils.miscfuncs import generic_checks
@@ -18,7 +19,11 @@ class theory(commands.Cog):
             async with await anyio.open_file("templates/theory.txt") as f:
                 words = [line.rstrip() for line in await f.readlines()]
                 word = random.choice(words)
-        await ctx.send("The " + word.title() + " " + random.choice(theories))
+        await ctx.send(
+            discord.utils.escape_mentions(
+                "The " + word.title() + " " + random.choice(theories)
+            )
+        )
 
 
 async def setup(client):
