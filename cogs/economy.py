4e6cdcd28f5c364cb2b420cb81094a100cb9f036
Bobbyperson <bobbyperson12345@gmail.com>
2025-09-21 04:05:52 -0400

    fix traceback printing

--- cogs/economy.py
+++ cogs/economy.py
@@ -41,6 +41,7 @@ getcontext().prec = 200
 
 log = logging.getLogger(__name__)
 
+
 class InventorySource(menus.ListPageSource):
     def __init__(self, data):
         super().__init__(data, per_page=25)
@@ -6098,7 +6099,7 @@ Roulette will end when everyone leaves the VC, or when the original invoker type
             )
             await channel.send(embed=embed)
             log.exception(error)
-            traceback.log.info_exception(
+            traceback.print_exception(
                 type(error), error, error.__traceback__, file=sys.stderr
             )
             await ctx.reply("An unexpected error occurred! Please try again.")
