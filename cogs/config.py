ef7fccea3c98d6dac35c1b8167a2c955d6cebdb7
Jan <sentrycraft123@gmail.com>
2025-09-22 20:30:15 +0200

    Implement key-value storage for runtime configuration (#164)
    Co-authored-by: Bobbyperson <bobbyperson12345@gmail.com>

---
+++ cogs/config.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+
+from utils import config
+
+
+class Config(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command=True)
+    @commands.is_owner()
+    async def config(self, ctx):
+        pass
+
+    @config.command(name="get")
+    async def get_config(self, ctx, key: str):
+        async with self.bot.session as session:
+            await ctx.send(repr(await config.get(session, key)))
+
+    @config.command(name="set")
+    async def set_config(self, ctx, key: str, *, value: str):
+        real_value = eval(value)
+
+        async with self.bot.session as session:
+            await config.set(session, key, real_value)
+
+async def setup(bot):
+    await bot.add_cog(Config(bot))
---
+++ utils/config.py
@@ -0,0 +1,37 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.config import Config
+
+
+async def get_raw(session: AsyncSession, key: str) -> any:
+    return (
+        await session.scalars(select(Config).where(Config.key == key))
+    ).one_or_none()
+
+async def get(session: AsyncSession, key: str) -> any:
+    entry = await get_raw(session, key)
+    if entry is not None:
+        entry = entry.value
+    return entry
+
+async def set(session: AsyncSession, key: str, value: any) -> None:
+    async with session.begin():
+        entry = await get_raw(session, key)
+
+        if value is None and entry is None:
+            pass
+
+        elif value is None:
+            await session.delete(entry)
+
+        elif entry is None:
+            session.add(
+                Config(
+                    key=key,
+                    value=value,
+                )
+            )
+
+        else:
+            entry.value = value
