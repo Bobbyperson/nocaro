fc75d219f1fa13ba11f1e633d4676225895a172c
Bobbyperson <bobbyperson12345@gmail.com>
2025-10-22 00:39:50 -0400

    walm bal image

--- cogs/economy.py
+++ cogs/economy.py
@@ -1909,6 +1909,8 @@ Example command: `,bougegram normal 100`"""
                 img = Image.open("templates/custombal/karma.jpg")
             if member.id == 402550402140340224:  # neko
                 img = Image.open("templates/custombal/neko.jpg")
+            if member.id == 924779672401764403:  # walm
+                img = Image.open("templates/custombal/walm.jpg")
         wid, hig = (500, 500)
         if amount == 1:
             msg = f"{amount} bouge buck"
