4cb1ea72e17d3569a996768204ca513627ca6ba8
Bobbyperson <bobbyperson12345@gmail.com>
2025-12-11 22:33:29 -0500

    add cans

--- cogs/image.py
+++ cogs/image.py
@@ -1,6 +1,8 @@
+import io
 import random as rd
 
 import aiohttp
+import discord
 from discord.ext import commands
 
 from utils.miscfuncs import generic_checks
@@ -33,6 +35,35 @@ class Image(commands.Cog):
                         await ctx.reply(dog["message"])
                     else:
                         await ctx.reply(f"Dog API failed. :( `{dog}`")
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(attach_files=True)
+    @commands.cooldown(1, 1, commands.BucketType.default)
+    @commands.max_concurrency(1, commands.BucketType.user, wait=True)
+    @generic_checks(max_check=False)
+    async def cans(self, ctx):
+        """The best command."""
+        if rd.randint(1, 100) == 1:
+            await ctx.reply("MEOW!")
+        else:
+            async with aiohttp.ClientSession() as session:
+                if rd.randint(1, 10) == 1:
+                    cat_url = "https://cataas.com/cat/gif"
+                    file = "cat.gif"
+                else:
+                    cat_url = "https://cataas.com/cat"
+                    file = "cat.jpg"
+                async with session.get(cat_url) as resp:
+                    if resp.status != 200:
+                        await ctx.send(
+                            f"Couldn't fetch a cat right now (status {resp.status}) :("
+                        )
+                        return
+
+                    data = await resp.read()
+
+        file = discord.File(io.BytesIO(data), filename=file)
+        await ctx.send(file=file)
 
 
 async def setup(client):
