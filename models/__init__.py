494be39a1c385e903f2dfec57f157627a25bf63b
Bobbyperson <bobbyperson12345@gmail.com>
2026-01-22 15:02:28 -0500

    add awards.py to init

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,23 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from . import achievements, api, config, database, economy, event, fire, osu, stocks
+from . import (
+    achievements,
+    api,
+    awards,
+    config,
+    database,
+    economy,
+    event,
+    fire,
+    osu,
+    stocks,
+)
 
 __all__ = [
     "achievements",
     "api",
+    "awards",
     "config",
     "database",
     "economy",
