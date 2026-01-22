d8ca59028218d4e0f88e3ce8bba9e6b6bfd8e0ce
Jan200101 <sentrycraft123@gmail.com>
2026-01-22 20:06:02 +0100

    betting

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,23 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from . import achievements, api, config, database, economy, event, fire, osu, stocks
+from . import (
+    achievements,
+    api,
+    betting,
+    config,
+    database,
+    economy,
+    event,
+    fire,
+    osu,
+    stocks,
+)
 
 __all__ = [
     "achievements",
     "api",
+    "betting",
     "config",
     "database",
     "economy",
